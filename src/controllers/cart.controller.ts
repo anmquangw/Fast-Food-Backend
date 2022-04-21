9b337a913e6a1099dabd1d28db69dbb72d41e4a9
Bùi Quang Hà <anmquangw@gmail.com>
2022-04-21 22:02:43 +0700

    make it better

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,19 +1,43 @@
 import { BaseController, Response, HttpStatus } from "../base/Controllers";
 import { ISelect, IUserData } from "../interfaces";
-import { Select as SelectModel } from "../models";
+import { Select as SelectModel, Food as FoodModel } from "../models";
 
 class Cart extends BaseController {
-  public list(req: any, res: Response): any {
+  public async list(req: any, res: Response): Promise<any> {
     const userInfo: IUserData = res.locals.payload;
     const idUser = userInfo._id;
 
     SelectModel.find({ idUser })
-      .then((data: any) => {
+      .then(async (data: any) => {
+        const foodIds: any = data.map((item: any) => item.idFood);
+        const foodList: any = await Promise.all(
+          (
+            await FoodModel.find({
+              _id: { $in: foodIds },
+            }).exec()
+          ).map(async (food: any) => {
+            const info = await food.toObject();
+            return {
+              idFood: info._id,
+              name: info.name,
+              quantityInStock: info.quantity,
+            };
+          })
+        );
+
+        data = data.map((item: any) => ({
+          ...item._doc,
+          ...foodList.find(
+            (food: any) => food.idFood.toString() === item.idFood.toString()
+          ),
+        }));
+
         return super.success(res, {
           data,
         });
       })
       .catch((error: any) => {
+        console.log(error);
         return super.failed(res, { error });
       });
   }
@@ -54,13 +78,11 @@ class Cart extends BaseController {
   public async update(req: any, res: Response): Promise<any> {
     const userInfo: IUserData = res.locals.payload;
     const idUser = userInfo._id;
-    const idFood = req.params.id;
+    const _id = req.params.id;
     const body: ISelect = req.body;
-    const oldData = await SelectModel.findOne({ idUser, idFood });
-    if (oldData) body.quantity += oldData.quantity;
 
     SelectModel.findOneAndUpdate(
-      { idUser, idFood },
+      { idUser, _id },
       [
         {
           $set: { quantity: body.quantity },
